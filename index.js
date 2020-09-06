5f7a052fd93b1b2c9543588ca40c89252c745b5c
SWFullPotential <swfullpotential@gmail.com>
2020-09-06 14:00:27 -0600

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ function fuzzyMatch(drivers, name){
     return drivers.filter(d => {return d.slice(0, sliced) === name})
 }
 
+// additional way to code with same result. 
+// function fuzzyMatch(source, testString) {
+//     return source.filter( possibleMatch =>
+//       possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+//     )
+//   }
+
 function matchName(drivers, string) {
     return drivers.filter(d => {return d.name === string})
 };
