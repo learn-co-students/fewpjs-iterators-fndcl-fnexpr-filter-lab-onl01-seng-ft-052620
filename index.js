1cbfdbdf293f7258e651fa33bfcf599ee489256a
Maloochow <luyao.mly@gmail.com>
2020-09-10 20:31:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter(driver => driver.toLowerCase() === string.toLowerCase()) 
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(driver => driver.charAt(0) === string.charAt(0))
+}
+
+function matchName(array, string) {
+    return array.filter(driver => driver.name === string)
+}
+
